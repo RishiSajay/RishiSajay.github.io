7da3fdd13fed70bd652ad5f823b7b3dcf5fd8bf6
rishisajay <73535575+rishisajay@users.noreply.github.com>
2022-06-25 22:26:34 -0400

    Added html, javascript, css, and extra files

---
+++ home_script.js
@@ -0,0 +1,171 @@
+//smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => 
+    {
+        anchor.addEventListener('click', function(e)
+        {
+            e.preventDefault();
+            document.querySelector(this.getAttribute("href")).scrollIntoView(
+            {
+                behavior : 'smooth' 
+            });
+            
+        });
+});
+/////////////////////////////////////////////////////////////////////////////////////
+
+//change navbar background based on position
+doc = document.documentElement;
+window.addEventListener("scroll", function(event)
+{
+    navbar = document.querySelector('.navbar');
+    aboutMe = document.querySelector('.about_me_container');
+    skills = document.querySelector('.skills_container');
+    projects = document.querySelector('.projects_container');
+    contact = this.document.querySelector('.contact_me_container');
+    var top  = this.scrollY;
+
+    if (top > 0)
+    {
+        navbar.classList.remove('aboutMe');
+    }
+    if (top > (aboutMe.getBoundingClientRect().top + this.scrollY - 80))
+    {
+        navbar.classList.remove('skills');
+        navbar.classList.add('aboutMe');
+    }
+    if (top > (skills.getBoundingClientRect().top + this.scrollY - 80))
+    {
+        navbar.classList.remove('aboutMe');
+        navbar.classList.remove('projects');
+        navbar.classList.add('skills');
+    }
+    if (top > (projects.getBoundingClientRect().top + this.scrollY - 80))
+    {
+        navbar.classList.remove('skills');
+        navbar.classList.remove('contact');
+        navbar.classList.add('projects');
+    }
+    if (top > (contact.getBoundingClientRect().top + this.scrollY - 80))
+    {
+        navbar.classList.remove('projects');
+        navbar.classList.add('contact');
+    }
+});
+/////////////////////////////////////////////////////////////////////////////////////
+
+//mobile navbar functionality
+const menu1 = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+menu1.addEventListener('click', function()
+{
+    menu1.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+//change background height based on lowest element location for each section. Useful to adjust to all screen sizes ie mobile, ipad, etc
+//intro
+intro = document.querySelector('.home_container');
+introCard = document.querySelector('.intro_card_container');
+
+//about me
+aboutMe = document.querySelector('.about_me_container');
+myPic = document.querySelector('.my_picture');
+
+//skills
+skills = document.querySelector('.skills_container');
+progress = document.querySelector('.progress_container');
+
+//projects
+projects = document.querySelector('.projects_container');
+lastProject = document.querySelector('.projects_card:nth-child(5)');
+
+//contact
+contact = document.querySelector('.contact_me_container');
+message = document.querySelector('.message_container');
+
+resizeHeight = function()
+{
+  height1 = (introCard.getBoundingClientRect().bottom + 200) - (intro.getBoundingClientRect().top);
+  intro.style.height = height1 + "px";
+
+  height2 = (myPic.getBoundingClientRect().bottom + 130) - (aboutMe.getBoundingClientRect().top);
+  aboutMe.style.height = height2 + "px";
+
+  height3 = (progress.getBoundingClientRect().bottom + 100) - (skills.getBoundingClientRect().top);
+  skills.style.height = height3 + "px";
+
+  height4 = (lastProject.getBoundingClientRect().bottom + 100) - (projects.getBoundingClientRect().top);
+  projects.style.height = height4 + "px";
+
+  height5 = (message.getBoundingClientRect().bottom + 70) - (contact.getBoundingClientRect().top);
+  contact.style.height = height5 + "px";
+}
+
+window.onload = resizeHeight;
+window.addEventListener("resize", resizeHeight);
+/////////////////////////////////////////////////////////////////////////////////////
+
+//info card functionality
+const card = document.querySelector('.intro_card');
+const text = document.querySelector('.text');
+
+card.addEventListener('click', function()
+{
+    card.classList.toggle('active');
+    text.classList.toggle('active');
+});
+/////////////////////////////////////////////////////////////////////////////////////
+ 
+
+
+//project card functionality
+const projectCards = document.querySelectorAll('.projects_card');
+const projectTexts = document.querySelectorAll('.projects_card p');
+
+for (let i = 0; i < projectCards.length; i++)
+{
+    projectCards[i].addEventListener('click', function()
+    {
+        projectCards[i].classList.toggle('active');
+        projectTexts[i].classList.toggle('active');
+    });
+}
+/////////////////////////////////////////////////////////////////////////////////////
+
+//contact me 
+//The formspree api is used to generate a message and send it to my email after the user clicks submit
+//This section removes the api success screen and puts my own success messages
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission! I will get back to you as soon as I can";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
---
+++ weather_script.js
@@ -0,0 +1,151 @@
+let weather = 
+{
+    apiKey: "3b90e0085d0660e288c5533afa4dc609",
+
+    //code for city data
+    fetchGeoPositionCity: function(city)
+    {
+        fetch
+        (
+            "http://api.openweathermap.org/geo/1.0/direct?q="
+            + city
+            + "&limit=1&appid="
+            + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.checkValidCityAndFetchWeather(data));
+    },
+
+    checkValidCityAndFetchWeather: function(data)
+    {
+        if(data === undefined || data.length == 0)
+        {
+            document.querySelector(".description").innerText = "Please enter a valid city name or zip code";
+            document.querySelector(".city").innerText = "";
+            document.querySelector(".icon").src = "";
+            document.querySelector(".temp").innerText = "";
+            document.querySelector(".humidity").innerText = "";
+            document.querySelector(".low").innerText = "";
+            document.querySelector(".high").innerText = "";
+        }
+        else
+        {
+            const {lat, lon} = data[0];
+            fetch
+            (
+                "https://api.openweathermap.org/data/2.5/weather?lat="
+                + lat
+                + "&lon="
+                + lon
+                + "&units=metric&appid="
+                + this.apiKey
+            )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+        }
+    },
+
+    //code for zip data
+    fetchGeoPositionZip: function(zip)
+    {
+        fetch
+        (
+            "http://api.openweathermap.org/geo/1.0/zip?zip="
+            + zip
+            + "&limit=1&appid="
+            + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.checkValidZipAndFetchWeather(data));
+    },
+
+    checkValidZipAndFetchWeather: function(data)
+    {
+        const {message} = data;
+        if(message === "not found")
+        {
+            document.querySelector(".description").innerText = "Please enter a valid city name or zip code";
+            document.querySelector(".city").innerText = "";
+            document.querySelector(".icon").src = "";
+            document.querySelector(".temp").innerText = "";
+            document.querySelector(".humidity").innerText = "";
+            document.querySelector(".low").innerText = "";
+            document.querySelector(".high").innerText = "";
+        }
+        else
+        {
+            const {lat, lon} = data;
+            fetch
+            (
+                "https://api.openweathermap.org/data/2.5/weather?lat="
+                + lat
+                + "&lon="
+                + lon
+                + "&units=metric&appid="
+                + this.apiKey
+            )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+        }
+    },
+
+    //display weather data 
+
+    displayWeather: function(data)
+    {
+        //note: name icon description etc are components of data and data.weather
+        const {name} = data;
+        //note: weather is an array
+        const {main, description, icon} = data.weather[0]; 
+        const {temp, temp_min, temp_max, humidity} = data.main;
+        
+        //change text
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".low").innerText = "Low: " + temp_min + "°C";
+        document.querySelector(".high").innerText = "High: " + temp_max + "°C";
+        
+        //change background
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + description + "')"
+
+        //Show contents
+        document.getElementsByClassName("weather")[0].style.visibility = "visible";
+    },
+
+    //search function
+    search: function()
+    {
+        var value = document.querySelector(".search-bar").value;
+        if ((value.replace(/ /g, "").length == 5) && !isNaN(parseInt(value)))
+        {
+            this.fetchGeoPositionZip((document.querySelector(".search-bar").value).replace(/ /g, ""));
+        }
+        else
+        {
+            this.fetchGeoPositionCity(document.querySelector(".search-bar").value);
+        }
+    },
+};
+
+//click search button
+document
+.querySelector(".search button")
+.addEventListener("click", function () 
+{
+    weather.search();
+});
+
+//press enter
+document
+.querySelector(".search-bar")
+.addEventListener("keyup", function(event)
+{
+    if (event.key == "Enter")
+    {
+        weather.search();
+    }
+});
