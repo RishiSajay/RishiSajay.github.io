a886190ae7a921bb94ae8f4d755be350c3297c08
rishisajay <73535575+rishisajay@users.noreply.github.com>
2022-06-26 01:29:57 -0400

    Update home_script.js

--- home_script.js
+++ home_script.js
@@ -76,7 +76,7 @@ myPic = document.querySelector('.my_picture');
 
 //skills
 skills = document.querySelector('.skills_container');
-progress = document.querySelector('.progress_container');
+skillsBody = document.querySelector('.skills_body_container');
 
 //projects
 projects = document.querySelector('.projects_container');
@@ -88,23 +88,14 @@ message = document.querySelector('.message_container');
 
 resizeHeight = function()
 {
-  if(window.innerWidth <= 500)
-  {
-    height1 = (introCard.getBoundingClientRect().bottom - 100) - (intro.getBoundingClientRect().top);
-    intro.style.height = height1 + "px";
-    height3 = (progress.getBoundingClientRect().bottom) - (skills.getBoundingClientRect().top);
-    skills.style.height = height3 + "px";
-  }
-  else
-  {
-    height1 = (introCard.getBoundingClientRect().bottom + 200) - (intro.getBoundingClientRect().top);
-    intro.style.height = height1 + "px";
-    height3 = (progress.getBoundingClientRect().bottom + 100) - (skills.getBoundingClientRect().top);
-    skills.style.height = height3 + "px";
-  }
+  height1 = (introCard.getBoundingClientRect().bottom + 200) - (intro.getBoundingClientRect().top);
+  intro.style.height = height1 + "px";
 
-  height2 = (myPic.getBoundingClientRect().bottom + 130) - (aboutMe.getBoundingClientRect().top);
+  height2 = (myPic.getBoundingClientRect().bottom + 100) - (aboutMe.getBoundingClientRect().top);
   aboutMe.style.height = height2 + "px";
+
+  height3 = (skillsBody.getBoundingClientRect().bottom + 100) - (skills.getBoundingClientRect().top);
+  skills.style.height = height3 + "px";
 
   height4 = (lastProject.getBoundingClientRect().bottom + 100) - (projects.getBoundingClientRect().top);
   projects.style.height = height4 + "px";
